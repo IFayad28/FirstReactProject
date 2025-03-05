cd51292dcd7466d172606cf765e7e4186149626a
Ihab Fayad <ihabzfayad@gmail.com>
2025-03-05 13:44:03 -0800

    Components Organized

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import React from 'react';
+import Navbar from './components/navbar.jsx';
+import Hero from './components/hero.jsx';
+import HomeCards from './components/homecards.jsx';
+import JobListings from './components/joblistings.jsx';
+import ViewAllJobs from './components/viewalljobs.jsx';
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <body>
+        <Navbar />
+        <Hero />
+        <HomeCards />
+        <JobListings />
+        <ViewAllJobs />
+      </body>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
